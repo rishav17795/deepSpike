ed6d6258709da62cc1292787ed5aac545be38d6c
Rishav Mukherjee <rishav.mukherjee@tum.de>
2021-12-01 13:03:59 +0100

    update .gitignore and .sh

--- runme.sh
+++ runme.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
-pip3 install numpy
+sudo apt-get install python3.8-dev python3.8-venv
+python3.8 -m venv lava_env
+source lava_env/bin/activate
+pip install -U pip
+cd lava
+pip install -r build-requirements.txt
+pip install -r requirements.txt
+export PYTHONPATH=$(pwd)/src
+pyb -E unit
+cd ..
+cd lava-dl
+pip install -r build-requirements.txt
+pip install -r requirements.txt
+export PYTHONPATH=$PYTHONPATH:$(pwd)/src
+pyb -E unit
+cd ..
+cd feature_extract
