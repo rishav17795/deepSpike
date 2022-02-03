524624bee672f0cd4fcae41283899782efc3aa12
Rishav Mukherjee <rishav.mukherjee@tum.de>
2022-02-03 13:41:12 +0100

    Test Rus code 2

--- feature_extract/rot2020_dataset.py
+++ feature_extract/rot2020_dataset.py
@@ -69,7 +69,7 @@ class ROTDataset(Dataset):
                     torch.zeros(2, self.w_in, self.w_in, self.num_time_bins).to(self.device),
                     sampling_time=self.sampling_time
                 )
-        # print("Events converted to tensor.")
+        print(f'Completed Loading:{label} from file\r')
         # spike_aug = roi_events_aug.fill_tensor(
         #             torch.zeros(2, self.w_in, self.w_in, self.num_time_bins).to(self.device),
         #             sampling_time=self.sampling_time
